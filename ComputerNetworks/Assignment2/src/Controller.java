9c98c1e2df13309c5c7322dab7435590833c9244
seangaff <gaffnese@gmail.com>
2021-11-08 15:02:21 +0000

    Assignment 2 skeleton

---
+++ ComputerNetworks/Assignment2/src/Controller.java
@@ -0,0 +1,11 @@
+import java.net.DatagramPacket;
+
+public class Controller extends Node {
+
+    @Override
+    public void onReceipt(DatagramPacket packet) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ ComputerNetworks/Assignment2/src/Router.java
@@ -0,0 +1,11 @@
+import java.net.DatagramPacket;
+
+public class Router extends Node{
+
+    @Override
+    public void onReceipt(DatagramPacket packet) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
